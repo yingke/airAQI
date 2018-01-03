eddf48b39f0135e56dbdf2262777bddde678a491
yingke <947098732@qq.com>
2018-01-04 00:39:30 +0800

    添加多线程 请求

--- Getdata.py
+++ Getdata.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-#获取2017年全年全国AQI日报指数
+# 获取2017年全年全国AQI日报指数
 #
 
 import requests
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 import pymongo
 import json
 import datetime
+import threading
 
 # 请求头信息
 headers = {
@@ -21,47 +22,52 @@ headers = {
     'Host': 'datacenter.mep.gov.cn:8099',
     'Origin': 'http://datacenter.mep.gov.cn:8099',
     'Referer': 'http://datacenter.mep.gov.cn:8099/ths-report/report!list.action',
-     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Mobile Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Mobile Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
 }
 
-url ='http://datacenter.mep.gov.cn:8099/ths-report/report!list.action';
+url = 'http://datacenter.mep.gov.cn:8099/ths-report/report!list.action'
 
 
 def main():
+    connection = pymongo.MongoClient('localhost', 27017)
+    tdb = connection.airdata
+    # 连接airdatas 集合 没有则自动创建
+    post = tdb.airdatas2
 
-    connection = pymongo.MongoClient('localhost', 27017);
-    ##连接airdata数据库，没有则自动创建
-    tdb = connection.airdata;
-    #连接airdatas 集合 没有则自动创建
-    post = tdb.airdatas;
-
-    # arrlen=12150;
-    # count = 406
-    arrlen=0
     count = 0
-    while(count<4445):
-        try:
-            params = {'page.pageNo': count, 'xmlname': 1462259560614, 'queryflag': 'close', 'V_DATE': '2017-01-01',
-                      'E_DATE': '2017-12-31'};
-            html = requests.post(url, data=params, headers=headers);
-            soup = BeautifulSoup(html.text, 'html.parser');
-            array = json.loads(soup.find(id="gisDataJson")["value"])
-            date = datetime.datetime.now().strftime('%H:%M:%S')
-            arrlen+=len(array)
-            print(u"第%s次运行 时间：%s (获取：%s数据)" % (count, date, arrlen))
+
+    while (count < 4445):
+        getdatas(count, post)
+        count += 1
 
 
-        except Exception as s:
-            print(s)
+def getdatas(i, post):
+    try:
+        arrlen = 0
+        params = {'page.pageNo': i, 'xmlname': 1462259560614, 'queryflag': 'close', 'V_DATE': '2017-01-01',
+                  'E_DATE': '2017-12-31'}
+        html = requests.post(url, data=params, headers=headers)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        array = json.loads(soup.find(id="gisDataJson")["value"])
+        date = datetime.datetime.now().strftime('%H:%M:%S')
+        arrlen += len(array)
+        print(u"第%s次运行 时间：%s (获取：%s数据)" % (i, date, arrlen))
 
-        try:
-            post.insert(array)
-            count+=1
-        except Exception as s:
-            print(s)
+    except Exception as s:
 
-        time.sleep(10)
+        print(s)
 
-if __name__=='__main__':
-    main()
+    try:
+        post.insert(array)
+        i += 1
+    except Exception as s:
+        print(s)
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=main)
+    t1.start()
+    # time.sleep的最小单位是毫秒
+    time.sleep(10)
